f1d6183a527c280fb3b1bd09027b6d2b8ae6435f
Victor Wolden West-Larsen <vict536f@edu.eal.dk>
2019-02-21 13:12:29 +0100

    Order class added

---
+++ SalesLib/Domain/Order.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalesLib.Model;
+
+namespace SalesLib.Domain
+{
+    class Order : IOrder
+    {
+        public ICustomer Customer { get; }
+
+        public DateTime DeliveryDate { get; }
+
+        public DateTime OrderDate { get; }
+
+        private List<IProductLineSaleItem> ProductItems { get; }
+
+        public int OrderID { get; }
+
+        public bool Picked { get; }
+
+        public List<IProductLineSaleItem> GetProductLineSaleItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order(ICustomer customer, DateTime deliveryDate)
+        {
+            Customer = customer;
+            deliveryDate = DeliveryDate;
+
+        }
+
+        public void AddProductLineSaleItem(IProductLineSaleItem productLineSaleItem)
+        {
+
+        }
+    }
+}
--- SalesLib/Model/IOrder.cs
+++ SalesLib/Model/IOrder.cs
@@ -9,10 +9,8 @@ namespace SalesLib.Model
     public interface IOrder
     {
         ICustomer Customer { get; }
-        DateTime DeliverDate { get; }
+        DateTime DeliveryDate { get; }
         DateTime OrderDate { get; }
-
-        List<IProductLineSaleItem> ProductItems { get; }
 
         int OrderID { get; }
         Boolean Picked { get; }
