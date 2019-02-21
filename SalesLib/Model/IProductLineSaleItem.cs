4a92a594689d64627261fd721e1dc60a78f90692
Victor Wolden West-Larsen <vict536f@edu.eal.dk>
2019-02-21 12:20:30 +0100

    Implemented IProductLineSaleItem

---
+++ SalesLib/Model/IProductLineSaleItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesLib.Model
+{
+    public interface IProductLineSaleItem
+    {
+        IProduct product { get; }
+        int Quantity { get; }
+        double Price { get; }
+        int LineID { get; }
+    }
+}
