070d86047ac15ea60e6ce01e53158bb4f3a1b925
Dupax <magnusj16@gmail.com>
2019-02-21 12:48:34 +0100

    Create Customer.cs

---
+++ SalesLib/Domain/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SalesLib.Model;
+
+namespace SalesLib.Domain
+{
+    class Customer : ICustomer
+    {
+        public string Name { get; }
+
+        public string Telephone { get; }
+
+        public string Address { get; }
+
+        public string Zip  { get; }
+
+        public string Town { get; }
+
+        public int CustomerID { get; }
+
+        public Customer(string name, string telephone, string zip, string address, string town, int customerID)
+        {
+            Name = name;
+            Telephone = telephone;
+            Address = address;
+            Zip = zip;
+            Town = town;
+            CustomerID = customerID;
+        }
+    }
+}
