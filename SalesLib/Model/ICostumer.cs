f4238615bc744d5e8255dc429fbc9326f13ececf
Molex <simplesleeptimer@gmail.com>
2019-02-21 12:04:30 +0100

    Add stuff

---
+++ SalesLib/Model/ICostumer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesLib.Model
+{
+    interface ICostumer
+    {
+        string Name { get; }
+        string Telephone { get; }
+        string Address { get; }   
+        string Zip { get; }
+        string Town { get; }
+        int CustomerID { get; }
+
+    }
+}
---
+++ SalesLib/Model/IOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesLib.Model
+{
+    interface IOrder
+    {
+        DateTime DeliverDate { get; }
+    }
+}
